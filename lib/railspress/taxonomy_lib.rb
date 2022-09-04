79f7aba8eab8f64ce0f100eca222f03a3933ed61
Justin Hamilton <justin.hamilton@hamiltondevco.com>
2022-09-04 00:49:03 -0400

    fix php mistakes

--- lib/railspress/taxonomy_lib.rb
+++ lib/railspress/taxonomy_lib.rb
@@ -438,7 +438,7 @@ module Railspress::TaxonomyLib
   end
 
   def get_post_type_object(post_type) # copied from PostsHelper, because it could not access it otherwise
-    if !RailspressPHP.is_scalar(post_type) || Railspress.GLOBAL.wp_post_types[post_type].blank?
+    if !Railspress::Php.is_scalar(post_type) || Railspress.GLOBAL.wp_post_types[post_type].blank?
       return nil
     end
     Railspress.GLOBAL.wp_post_types[post_type]
